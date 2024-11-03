dde62a51889dccb84a8cc537054f5f8eb79b85a9
Khanak21 <khanakpatwari21@gmail.com>
2024-11-03 19:55:15 +0530

    added persistence in store

--- src/app/api/team/route.ts
+++ src/app/api/team/route.ts
@@ -2,19 +2,25 @@ import User from "@/models/userModel";
 import Team from "@/models/teamModel";
 import { NextRequest, NextResponse } from "next/server";
 import { connect } from "@/dbConfig/dbConfig";
+import mongoose from "mongoose";
 
 // GET all teams of user
 export async function GET(request:NextRequest){
     try{
-       const userId = await request.json()
+        const {searchParams} = new URL(request.url)
+        const userId = searchParams.get('userId') 
        await connect()
+       if(mongoose.Types.ObjectId.isValid(userId)){
        const user = await User.findById(userId)
 
        if(!user)return NextResponse.json({error:"User does not exist"},{status:400})
 
        const teamsList = user.teams
-       const teams = await Team.find({_id:{$in:{teamsList}}})
+       const teamIds = user.teams.map((team: { teamId: any; }) => team.teamId);
+       const teams = await Team.find({_id:{$in:teamIds}})
+    //    console.log(user.teams)
        return NextResponse.json({teams:teams,success:true},{status:200})
+       }
     }catch(error:any){
         return NextResponse.json({error:error.message},{status:500})
     }
@@ -25,14 +31,18 @@ export async function POST(request:NextRequest){
     try{
        const {team,userId} = await request.json()
        await connect()
+       if(mongoose.Types.ObjectId.isValid(userId)){
        let newTeam = new Team(team)
        newTeam = await newTeam.save()
 
-       const user = await User.findByIdAndUpdate(userId,{$push:{teams:{teamId:newTeam._id,joinedAt:Date.now}}})
+       const user = await User.findByIdAndUpdate(userId,{$push:{teams:{teamId:newTeam._id,joinedAt:Date.now()}}},{new:true})
 
        if(!user)return NextResponse.json({error:"User does not exist"},{status:400})
-
-       return NextResponse.json({teams:user.teams,success:true},{status:200})
+       
+       return NextResponse.json({teams:user.teams,team:newTeam,success:true},{status:200})
+       }else{
+        return NextResponse.json({error:"Invalid user"},{status:500})
+       }
     }catch(error:any){
         return NextResponse.json({error:error.message},{status:500})
     }
